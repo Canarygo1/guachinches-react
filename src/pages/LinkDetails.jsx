f4c5c5bd34a1612cbb518a851a5ff4b822c60378
Canarygo1 <33827581+Canarygo1@users.noreply.github.com>
2023-11-07 21:22:46 +0000

    commit:carta finish

--- src/pages/LinkDetails.jsx
+++ src/pages/LinkDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {IconButton} from "@mui/material";
 import {Box} from "@material-ui/core";
+import {useHistory} from "react-router-dom";
 
 const videoStyle = {
     position: 'fixed',
@@ -11,6 +12,7 @@ const videoStyle = {
     objectFit: 'cover',
     zIndex: -1,
 };
+
 const buttonContainerStyle = {
     display: 'flex',
     flexDirection: 'column', // Columna en lugar de fila
@@ -39,6 +41,7 @@ const buttonStyle = {
 };
 const googleLogo = '../google_icon.png'; // Reemplaza con la URL de tu imagen
 const instagramLogo = '../instagram_icon.png'; // Reemplaza con la URL de tu imagen
+const menuLogo = '../menu_icon.png'; // Reemplaza con la URL de tu imagen
 const facebookLogo = '../facebook_icon.png'; // Reemplaza con la URL de tu imagen
 const whatsappLogo = '../whatsapp_app_icon.png'; // Reemplaza con la URL de tu imagen
 const businessLogo = '../la_maestra.jpeg'; // Reemplaza con la URL de tu imagen
@@ -46,12 +49,13 @@ const videoUrl = 'https://orsay.s3.fr-par.scw.cloud/384734274_1688471565007414_8
 const googleUrl = 'https://search.google.com/local/writereview?placeid=ChIJr4F_AjyfagwRdQ9-WQytfqM'
 const facebookUrl = 'https://www.facebook.com/guachinchelamaestra/?locale=es_LA'
 const instagramUrl = 'https://www.instagram.com/guachinchelamaestra/'
-const whatsAppUrl = 'https://wa.me/?text=Valora el guachinche la maestra en el siguiente link: '+googleUrl
+const whatsAppUrl = 'https://wa.me/?text=Valora el guachinche la maestra en el siguiente link: ' + googleUrl
 
 const LinkDetails = () => {
     const handleButtonClick = (href) => {
         window.open(href, '_blank'); // Replace with your external link
     };
+    const history = useHistory();
 
     return (
         <div>
@@ -59,16 +63,16 @@ const LinkDetails = () => {
                 <source src={videoUrl} type="video/mp4"/>
             </video>
             <div>
-                    <div style={buttonContainerStyle}>
-                        <div style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            gap: 18,
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            weight: '100%',
-                            height: '100%',
-                        }}>
+                <div style={buttonContainerStyle}>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 18,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        weight: '100%',
+                        height: '100%',
+                    }}>
 
                         <div style={{
                             display: 'flex',
@@ -86,50 +90,70 @@ const LinkDetails = () => {
                                 color: 'white',
                             }}>Guachinche la maestra</h1>
                         </div>
-
-                        <IconButton style={buttonStyle} onClick={()=>handleButtonClick(googleUrl)}>
+                        <IconButton style={{...buttonStyle, backgroundColor: 'white', color: '#0085C4'}}
+                                    onClick={() => history.push('/link/guachinche_la_maestra/menu')}>
+                            <div style={{
+                                display: 'flex',
+                                width: '100%',
+                                justifyContent: 'center',
+                            }}>
+                                Ver carta
+                            </div>
+                        </IconButton>
+                        <IconButton sx={{...buttonStyle,backgroundColor:'main.priary'}} onClick={() => handleButtonClick(googleUrl)}>
                             <img src={googleLogo} alt="Logo" style={{width: '30px', marginRight: '10px'}}/>
                             Escribe tu valoración
                         </IconButton>
-                        <IconButton style={{...buttonStyle,backgroundColor:'white',color:'#0085C4'}} onClick={()=>handleButtonClick(whatsAppUrl)}>
-                            <img src={whatsappLogo} alt="Logo" style={{width: '30px', marginRight: '10px',
-                                objectFit: 'cover'}}/>
+
+                        <IconButton style={{...buttonStyle, backgroundColor: 'white', color: '#0085C4'}}
+                                    onClick={() => handleButtonClick(whatsAppUrl)}>
+                            <img src={whatsappLogo} alt="Logo" style={{
+                                width: '30px', marginRight: '10px',
+                                objectFit: 'cover'
+                            }}/>
 
                             Comparte para valorar
                         </IconButton>
-                        <IconButton style={{...buttonStyle,backgroundColor:'white',color:'#0085C4'}} onClick={()=>handleButtonClick(instagramUrl)}>
+                        <IconButton style={{...buttonStyle, backgroundColor: 'white', color: '#0085C4'}}
+                                    onClick={() => handleButtonClick(instagramUrl)}>
                             <img src={instagramLogo} alt="Logo" style={{width: '30px', marginRight: '10px'}}/>
-                            Siguenos en Instagram
+                            <div style={{
+                                fontSize: '23px',
+                            }}>Síguenos en Instagram
+                            </div>
                         </IconButton>
-                        </div>
-                        <Box style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'end',
-                            alignItems: 'center',
-                            height: '30%',
-                            padding:0,
-                            margin:0,
-                            color: 'white',
-                        }}>
-                            <img src={'../app_icon.png'} alt="Logo" style={{width: '42px', marginRight: '10px'}}/>
-                            <p style={{
-                                fontWeight: 'bold',
-                                marginBottom: '8px',
-                            }}>Donde Comer Canarias</p>
-                            <p style={{
-                                marginTop: '0px',
-                                marginBottom: '8px',
-
-                            }} onClick={()=>handleButtonClick('https://apps.apple.com/tr/app/donde-comer-canarias/id1575882373')}>Descarga nuestra app <span style={{
-                                textDecoration: 'underline',
-                            }}>aquí</span> </p>
-                        </Box>
 
                     </div>
+                    <Box style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'end',
+                        alignItems: 'center',
+                        height: '30%',
+                        padding: 0,
+                        margin: 0,
+                        color: 'white',
+                    }}>
+                        <img src={'../app_icon.png'} alt="Logo" style={{width: '42px', marginRight: '10px'}}/>
+                        <p style={{
+                            fontWeight: 'bold',
+                            marginBottom: '8px',
+                        }}>Donde Comer Canarias</p>
+                        <p style={{
+                            marginTop: '0px',
+                            marginBottom: '8px',
 
+                        }}
+                           onClick={() => handleButtonClick('https://apps.apple.com/tr/app/donde-comer-canarias/id1575882373')}>Descarga
+                            nuestra app <span style={{
+                                textDecoration: 'underline',
+                            }}>aquí</span></p>
+                    </Box>
 
                 </div>
+
+
+            </div>
         </div>
     );
 };
