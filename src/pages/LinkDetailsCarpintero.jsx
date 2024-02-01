ac25599baa8c8b71934395b00e12153a48f0b3f1
Canarygo1 <33827581+Canarygo1@users.noreply.github.com>
2024-02-01 17:37:23 +0000

    commit:carta finish

--- src/pages/LinkDetailsCarpintero.jsx
+++ src/pages/LinkDetailsCarpintero.jsx
@@ -66,6 +66,7 @@ const LinkDetails = () => {
                 loop
                 playsInline
                 muted
+                autoPlay
                 style={videoStyle}
             >
                 <source src={videoUrl} type="video/mp4" />
